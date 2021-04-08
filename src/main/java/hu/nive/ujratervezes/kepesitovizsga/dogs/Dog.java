3f65d8143dcf26874b383425bbdd31f89f23b3c5
wakesz87 <kisszab87@gmail.com>
2021-04-08 11:10:16 +0200

    Dogs

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/dogs/Dog.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/dogs/Dog.java
@@ -1,4 +1,59 @@
 package hu.nive.ujratervezes.kepesitovizsga.dogs;
 
 public class Dog {
+
+    private String name;
+    private String section;
+    private String provisional;
+    private String country;
+    private String url;
+    private String image;
+    private String pdf;
+
+    public Dog(String name, String section, String provisional, String country, String url, String image, String pdf) {
+        this.name = name;
+        this.section = section;
+        this.provisional = provisional;
+        this.country = country;
+        this.url = url;
+        this.image = image;
+        this.pdf = pdf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getProvisional() {
+        return provisional;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPdf() {
+        return pdf;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
 }
--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/dogs/DogManager.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/dogs/DogManager.java
@@ -1,5 +1,77 @@
 package hu.nive.ujratervezes.kepesitovizsga.dogs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class DogManager {
+
+    public List<Dog> getCountryByExactDogSpecies(String name) {
+        List<Dog> dogList = new ArrayList<>(fileReader());
+        List<Dog> result = new ArrayList<>();
+        for (Dog search : dogList) {
+            if (search.getName().equals(name.toLowerCase())) {
+                result.add(search);
+            }
+        }
+        return result;
+    }
+
+    public Map<String, Integer> getDogStatistics() {
+        Map<String, Integer> result = new HashMap<>();
+        String line;
+        try (BufferedReader is =
+                     new BufferedReader(
+                             new InputStreamReader(Dog.class.getResourceAsStream("/dogs.csv")))) {
+            is.readLine();
+            while ((line = is.readLine()) != null) {
+                String[] temp = line.split(";");
+                String key = temp[4];
+                if (!result.containsKey(key)) {
+                    result.put(key, 1);
+                } else {
+                    int act = result.get(key);
+                    result.replace(key, act + 1);
+                }
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            throw new IllegalArgumentException("File not found", ioe);
+        }
+        return result;
+    }
+
+
+    public List<Dog> getDogsInAlphabeticalOrderByName() {
+        List<Dog> result = new ArrayList<>(fileReader());
+        Collections.sort(result, new Comparator<Dog>() {
+            public int compare(Dog o1, Dog o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        return result;
+    }
+
+    private List<Dog> fileReader() {
+        List<Dog> dogs = new ArrayList<>();
+        String line;
+        try (BufferedReader is =
+                     new BufferedReader(
+                             new InputStreamReader(Dog.class.getResourceAsStream("/dogs.csv")))) {
+            is.readLine();
+            while ((line = is.readLine()) != null) {
+                String[] temp = line.split(";");
+                if (temp.length==8){
+                    dogs.add(new Dog(temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], temp[7]));}
+                else{System.out.println(temp[0] + ": " + temp.length);}
+            }
+            return dogs;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            throw new IllegalArgumentException("File not found", ioe);
+        }
+    }
+
 
 }
